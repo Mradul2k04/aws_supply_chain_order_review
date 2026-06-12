cf36f6df5bc05ee8d7e3b4a6c2eee8dfad9d3575
Mradul2k04 <mradul.btech.cs22@gamil.com>
2026-06-12 22:37:24 +0530

    [Mradul Sharma] Updated the task

--- sqlalchemy/database.py
+++ sqlalchemy/database.py
@@ -1,10 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/aws_supply_chain"
+DATABASE_URL = "postgresql://postgres:Mradulk@123@localhost:5432/aws_supply_chain"
 
-
-engine = create_engine(DATABASE_URL,connect_args={'check_same_thread':False},pool_size=10,max_overflow=20)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True,pool_size=10,max_overflow=20)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
