0320ee4c7056e5fb95b8823543501fd6e371e34d
Nisha Chaudhary <nishachaudhary91101@gmail.com>
2026-06-12 22:21:16 +0530

    [Nisha Chaudhary] added main file by updating of including routes

--- sqlalchemy/main.py
+++ sqlalchemy/main.py
@@ -1,5 +1,12 @@
+from fastapi import FastAPI
 from database import engine, get_db
 import orm_models
 
+from router import router
+
+app = FastAPI()
+
+# Include all routes
+app.include_router(router)
 
 orm_models.Base.metadata.create_all(bind=engine)
